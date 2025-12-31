a2b252a32b7a3734623be9ecc80b11af42833b93
premlata15 <chaudharymuskan94@gmail.com>
2025-12-31 16:36:18 +0545

    features added

---
+++ src/components/landingComponents/Features.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Features() {
+  return (
+    <section>
+      <div>
+        <h2 className="text-4xl font-bold mb-6 text-center mb-10">Features</h2>
+        <p className="text-center text-gray-600">
+          Discover the amazing features that make WanderWise your perfect travel
+          companion.
+        </p>
+      </div>
+    </section>
+  );
+}
+
+export default Features;
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "../components/landingComponents/Navbar";
 import Hero from "../components/landingComponents/Hero";
+import Features from "../components/landingComponents/Features";
 
 const LandingPage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Features />
     </>
   );
 };
